e895b9a7c2ff9cea568e42a7dbaed9fc57bda676
Ken <gialap2002@gmail.com>
2022-04-20 09:33:41 +0700

    feat: add footer

--- league-of-legends/src/context/menu-context/context.tsx
+++ league-of-legends/src/context/menu-context/context.tsx
@@ -1,30 +1,30 @@
-import React, { createContext, FC, ReactNode } from "react";
+import React, { createContext, FC } from "react";
 import { menuItems } from "../../common/ui/layout/main-layout/model";
 
-interface Props {
-  children: ReactNode;
+interface MenuContextInterface {
+  items: Array<menuItems>;
 }
 
-const MenuContext = createContext({
-  items: Array<menuItems>([]),
-});
+const MenuContext = createContext<MenuContextInterface>({ items: [] });
 
-export const MenuContextProvider: FC<Props> = (props: Props) => {
-  const menuItems = [
-    { title: "shortcut.champions", url: "/champions" },
-    { title: "shortcut.regions" },
-    {
-      title: "shortcut.altUniverse",
-      items: ["universe.starGuardian", "universe.odyssey", "universe.k/da"],
-    },
-    { title: "shortcut.comics" },
-    { title: "shortcut.map" },
-    { title: "shortcut.explore" },
-    { title: "shortcut.search" },
-  ];
+export const MenuContextProvider: FC<{}> = (props) => {
+  const menuItemsContext: MenuContextInterface = {
+    items: [
+      { title: "shortcut.champions", url: "/champions" },
+      { title: "shortcut.regions" },
+      {
+        title: "shortcut.altUniverse",
+        items: ["universe.starGuardian", "universe.odyssey", "universe.k/da"],
+      },
+      { title: "shortcut.comics" },
+      { title: "shortcut.map" },
+      { title: "shortcut.explore" },
+      { title: "shortcut.search" },
+    ],
+  };
 
   return (
-    <MenuContext.Provider value={{ items: menuItems }}>
+    <MenuContext.Provider value={menuItemsContext}>
       {props.children}
     </MenuContext.Provider>
   );
